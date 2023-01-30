69e47d5bae3f75e7bca3afc8313789e8c743ab83
Victoria Shirley <102524394+VictoriaShirley@users.noreply.github.com>
2023-01-30 14:37:27 +0000

    dark mode

---
+++ assets/scripts/btn.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector('.btn');
+const body = document.querySelector('body')
+
+btn.onclick = function ()
+  {
+    this.classList.toggle('active')
+    body.classList.toggle('active')
+  }
